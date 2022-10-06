705f76be914654c59645df4f6ee46b8567f31b52
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-06 10:58:06 -0700

    Add files via upload

---
+++ cleansingservices.js
@@ -0,0 +1,85 @@
+// Marvish Chandra
+
+class cleansingServices{
+    function facials(){
+        fall_renewal_facial_special = {Time: 50, Price: 125, Description: "Fall back in love with your skin! Treat yourself with a Pumpkin Spice inspired experience your skin will thank you for. This highly personalized facial made just for you will left smoother, brighter, & nourished."};
+        refreshFacial = {Time: 25, Price: 80, Description: "Great for all skin types, this is refreshing introduction to facial treatments includes skin analysis, cleansing, and exfoliate. This facial has does not come with extractions."};
+        fiftymin_customfacial = {Time: 50, Price: 150, Description: "Just for you! A specialized treatment completely customized to provide optimal results for every skin type. This facial also includes a masque and a relaxing neck and shoulder massage."};
+        fiftymin_intensehydratingfacial = {Time: 50, Price: 150, Description: "Using customized hydration for your skin type, this facial offers gentle cleansing, exfoliation, and cream application to nourish and hydrate dry skin."};
+        fiftymin_sensitiveskinfacial = {Time: 50, Price: 150, Description: "Designed for sensitive skin, this facial uses gentle healing techniques to help soothe, protect and rebuild skin affected by the environment, rosacea, medical peels, and laser treatments."};
+        eightymin_antiagingfacial = {Time: 80, Price: 250, Description: "This highly effective facial reduces the appearance of fine lines and wrinkles while hydrating, strengthening, and visibly firming the skin."};
+        eightymin_customfacial = {Time: 80, Price: 235, Description: "Just for you! A specialized treatment completely customized to provide optimal results for every skin type. This facial also includes a masque and a relaxing neck and shoulder massage."};
+        eightymin_intensehydratingfacial = {Time: 80, Price: 235, Description:"Using customized hydration for your skin type, this facial offers gentle cleansing, exfoliation, and cream application to nourish and hydrate dry skin."};
+        eightymin_sensitiveskinfacial = {Time: 80, Price: 235, Description:"Designed for sensitive skin, this facial uses gentle healing techniques to help soothe, protect and rebuild skin affected by the environment, rosacea, medical peels, and laser treatments."};
+    }
+    function massages(){
+        fall_swediesh_massage_special = {Time: 50, Price: 140, Description:"Swedish techniques during this popular full-body massage with essential Pumpkin Spice oil to relax your entire body and improve circulation. This treatment uses light to medium pressure."};
+        hundredmin_aromatherapy = {Time: 100, Price: 320, Description:"Our aromatherapy massage uses relaxing Swedish techniques combined with essential oil to help uplift, calm & balance your energy. This experience will ease fatigue and exhaustion while soothing the over-stressed nervous system. Choose the aromatherapy scent that you like best from a variety of essential oils such as Peppermint, Eucalyptus and Warm Lavender, Tea Tree, Lemongrass, and Sweet Orange for this Swedish massage."};
+        hundredmin_cbdwellness = {Time: 100, Price: 360, Description:"The calming, soothing effects of CBD work in unison with massage techniques designed to quiet the mind and relax the body. Perfect for anyone seeking a blissful escape. This treatment will elevate your mind, relax the nervous system, and help provide relief from aches, pains, sore muscles, arthritis, sprains, strains, and inflammation."};
+        hundredmin_swedish_massage = {Time: 100, Price: 300, Description:"Swedish massage is designed to provide optimal results for your body’s unique needs. This treatment uses light to medium strokes and therapeutic techniques helps fight fatigue, release tension, and restore vitality."};
+        twentyfivemin_CBDfootmassage = {Time: 25, Price: 90, Description:"Take your Foot treatment to an even deeper level with the therapeutic benefits of CBD!"};
+        scalp_neckshouldercbd = {Time: 25, Price: 90, Description:"Take your head, neck, and shoulder Swedish massage to an even deeper level with the therapeutic benefits of CBD! This massage targets overworked, stressed muscle in the head, neck & shoulder areas."};
+        fiftymin_birthdaycelebration_massage = {Time: 50, Price: 180, Description:"It doesn't need to be your birthday to celebrate with this Swedish massage, leaving your skin smelling sweet with a rejuvenating little foot scrub. REMARKS, Products used: Lalicious Birthday Cake Massage Oil and Sugar Kiss Scrub. **Guest will receive a birthday gift at end of service from service provider."};
+        fiftymin_cbdwellness_massage = {Time: 50, Price: 180, Description:"The calming, soothing effects of CBD work in unison with massage techniques designed to quiet the mind and relax the body. Perfect for anyone seeking a blissful escape. This treatment will elevate your mind, relax the nervous system, and help provide relief from aches, pains, sore muscles, arthritis, sprains, strains, and inflammation."};
+        fiftymin_deeptissue_massage = {Time: 50, Price: 160, Description:"Surrender to Eforea as slow, deliberate massage techniques using elbows, forearms and knuckles relieve tightness from the overuse and tension of muscles. Deep tissue is popular among athletes and frequent massage goers, who prefer a firm touch to a gentle one."};
+        fiftymin_peainthepod_massage = {Time: 50, Price: 150, Description:"Beginning in the second trimester and throughout the remainder of the pregnancy, this specialized massage eases soreness and pain, while allowing for a moment of much needed relaxation. Must be in second and third trimester!"};
+        fiftymin_sports_massage = {Time: 50, Price: 165, Description:"A customized deep pressure massage designed to relax and optimize muscle response. Deep kneading eases tension while stretching and mobility enhancing techniques promote flexibility and healing for increased recovery time."};
+        fiftymin_swedish_massage = {Time: 50, Price: 150, Description:"Swedish massage is designed to provide optimal results for your body’s unique needs. This treatment uses light to medium strokes and therapeutic techniques helps fight fatigue, release tension, and restore vitality."};
+        fiftymin_aromatherapy_massage = {Time: 50, Price: 160, Description:"Our aromatherapy massage uses relaxing Swedish techniques combined with essential oil to help uplift, calm & balance your energy. This experience will ease fatigue and exhaustion while soothing the over-stressed nervous system. Choose the aromatherapy scent that you like best from a variety of essential oils such as Peppermint, Eucalyptus and Warm Lavender, Tea Tree, Lemongrass, and Sweet Orange for this Swedish massage."};
+        eightymin_cbdwellness_massage = {Time: 80, Price: 265, Description:"The calming, soothing effects of CBD work in unison with massage techniques designed to quiet the mind and relax the body. Perfect for anyone seeking a blissful escape. This treatment will elevate your mind, relax the nervous system, and help provide relief from aches, pains, sore muscles, arthritis, sprains, strains, and inflammation."};
+        eightymin_deeptissue_massage = {Time: 80, Price: 245, Description:"Surrender to Eforea as slow, deliberate massage techniques using elbows, forearms and knuckles relieve tightness from the overuse and tension of muscles. Deep tissue is popular among athletes and frequent massage goers, who prefer a firm touch to a gentle one."};
+        eighymin_desertstone_massage = {Time: 80, Price: 255, Description:"Take time to relax with this massage using the therapeutic properties of heated basalt stones and Swedish massage strokes to penetrate tight muscles, improve circulation, and promote a deep sense of well-being."};
+        eightymin_peainthepod_massage = {Time: 80, Price: 235, Description:"Beginning in the second trimester and throughout the remainder of the pregnancy, this specialized massage eases soreness and pain, while allowing for a moment of much needed relaxation. Must be in second and third trimester!"};
+        eightymin_sports_massage = {Time: 80, Price: 250, Description:"A customized deep pressure massage designed to relax and optimize muscle response. Deep kneading eases tension while stretching and mobility enhancing techniques promote flexibility and healing for increased recovery time."};
+        eightymin_swedish_massage = {Time: 80, Price: 235, Description:"Swedish massage is designed to provide optimal results for your body’s unique needs. This treatment uses light to medium strokes and therapeutic techniques helps fight fatigue, release tension, and restore vitality."};
+        eightymin_aromatherapy_massage = {Time: 80, Price: 245, Description:"Our aromatherapy massage uses relaxing Swedish techniques combined with essential oil to help uplift, calm & balance your energy. This experience will ease fatigue and exhaustion while soothing the over-stressed nervous system. Choose the aromatherapy scent that you like best from a variety of essential oils such as Peppermint, Eucalyptus and Warm Lavender, Tea Tree, Lemongrass, and Sweet Orange for this Swedish massage."};
+        couplesfiftymin_cbdwellness_massage = {Time: 50, Price: 200, Description:"This service is $200.00 per person!"};
+        couplesfiftymin_deeptissue_massage = {Time: 50, Price: 180, Description:"This service is $180.00 person!"};
+        couplesfiftymin_sports_massage = {Time: 50, Price: 185, Description:"This service is $185.00 per person!"};
+        couplesfiftymin_swedish_massage = {Time: 50, Price: 170, Description:"This service is $170.00 per person!"};
+        couplesfiftymin_aromatherapy_massage = {Time: 50, Price: 180, Description:"This servie is $180.00 per person!"};
+        coupleseightymin_cdbwellness_massage = {Time: 80, Price: 285, Description:"This service is $285.00 per person!"};
+        coupleseightymin_deeptissue_massage = {Time: 80, Price: 265, Description:"This service is $265.00 per person!"};
+        coupleseightymin_desertstone_massage = {Time: 80, Price: 275, Description:"This service is $275.00 per person!"};
+        coupleseightymin_sports_massage = {Time: 80, Price: 270, Description:"This service is $270.00 per person!"};
+        coupleseightymin_swedish_massage = {Time: 80, Price: 255, Description:"This service is $250.00 per person!"};
+        coupleseightymin_aromatherapy_massage = {Time: 80, Price: 265, Description:"This service is $265.00 per person!"};
+
+    }
+    function nails(){
+        fall_renewal_manicure = {Time: 50, Price: 70, Description:"Treat your hands with a soothing pumpkin scrub and moisturizing lotion. This service includes soak, trim, cuticle clean up, and polish of your choice."};
+        fall_renewal_pedicure = {Time: 50, Price: 90, Description:"Treat your feet with a soothing pumpkin scrub and moisturizing lotion. This service includes soak, trim, cuticle clean up, and polish of your choice."};
+        cbd_manicure_fiftymin = {Time: 50, Price: 75, Description:"Experience a CBD Manicure designed to leave the hands exfoliated and moisturized after an exceptionally nourishing and soothing massage treatment. This treatment includes an ultra-hydrating pressure point massage, cuticle maintenance and polish. plus a glass of bubbly."};
+        cbd_pedicure_fiftymin = {Time: 50, Price: 95, Description:"Experience a CBD Pedicure designed to leave the feet exfoliated and moisturized after an exceptionally nourishing and soothing massage treatment. This treatment includes an ultra-hydrating pressure point massage, cuticle maintenance and polish. plus a glass of bubbly."};
+        classic_manicure_fiftymin = {Time: 50, Price: 65, Description:"Exfoliating fruit extracts and flower essence infused body polish, detoxify, soften and revitalize the skin. This classic treatment includes an ultra-hydrating pressure point massage, exceptional cuticle maintenance and polish."};
+        classic_pedicure_fiftymin = {Time: 50, Price: 50, Description:"Exfoliating fruit extracts and flower essence infused body polish, detoxify, soften and revitalize the skin. This classic treatment includes an ultra-hydrating pressure point massage, exceptional cuticle maintenance and polish."};
+        gelMancicure = {Time: 50, Price: 75, Description:"This is our Signature Manicure, with upgraded Gel Polish included! No drying time, chipping or smudging! Gel polish manicures use a gel coating instead of regular polish, resulting in incredible shine, depth of color and perfect finish. Gel polish manicures provide excellent wear, lasting up to 14 days."};
+        gelPedicure = {Time: 50, Price: 90, Description:"This is our Signature Pedicure, with upgraded Gel Polish included! No drying time, chipping or smudging! Gel polish pedicures use a gel coating instead of regular polish, resulting in incredible shine, depth of color and perfect finish. Gel polish pedicures provide excellent wear, lasting up to 14 days."};
+        gelpolishes_removal_soakoff = {Time: 15, Price: 20};
+        indulgence_manicure_seventyfivemin = {Time: 75, Price: 85, Description:"This luxurious treatment includes exhilarating botanical ingredients and stimulating aromatherapy. A detoxifying soak, refreshing eucalyptus scrub and mask, and enriched exotic oils hydrate and soften skin. Finish with paraffin, polish, and a glass of bubbly."};
+        indulgence_pedicure_seventyfivemin = {Time: 75, Price: 110, Description:"This luxurious treatment includes exhilarating botanical ingredients and stimulating aromatherapy. A detoxifying soak, refreshing eucalyptus scrub and mask, and enriched exotic oils hydrate and soften skin. Finish with paraffin, polish, and a glass of bubbly."};
+        
+    }
+    function waxing(){
+        backWax = {Time: 60, Price: 115, Description:"*Please note - In order to receive Waxing service all body hair must be no less that a 1/4 inch. If trimming is required that is an additional cost."};
+        bikiniWaxing = {Time: 30, Price: 65, Description:" * Please note - In order to receive Waxing service all body hair must be no less that a 1/4 inch. If trimming is required that is an additional cost."};
+        brazilianWax = {Time: 40, Price: 95, Description:" * Please note - In order to receive Waxing service all body hair must be no less that a 1/4 inch. If trimming is required that is an additional cost."};
+        browWax = {Time: 15, Price: 35, Description:"* Please note - In order to receive Waxing service all body hair must be no less that a 1/4 inch. If trimming is required that is an additional cost."};
+        chinWaxing = {Time: 15, Price: 35, Description:"* Please note - In order to receive Waxing service all body hair must be no less that a 1/4 inch. If trimming is required that is an additional cost."};
+        full_chest_waxing = {Time: 60, Price: 65, Description:"* Please note - In order to receive Waxing service all body hair must be no less that a 1/4 inch. If trimming is required that is an additional cost."};
+        full_leg_wax = {Time: 60, Price: 115, Description:"* Please note - In order to receive Waxing service all body hair must be no less that a 1/4 inch. If trimming is required that is an additional cost."};
+        halfArms = {Time: 15, Price: 55, Description:" * Please note - In order to receive Waxing service all body hair must be no less that a 1/4 inch. If trimming is required that is an additional cost."};
+        lipWaxing = {Time: 15, Price: 20, Description:"* Please note - In order to receive Waxing service all body hair must be no less that a 1/4 inch. If trimming is required that is an additional cost."};
+        underarmWaxing = {Time: 15, Price: 35, Description:"* Please note - In order to receive Waxing service all body hair must be no less that a 1/4 inch. If trimming is required that is an additional cost."};
+
+    }
+    function packages(){
+        abrigter_youawaits = {Time: 75, Price: 170, Description:"25 minute Refresh, Facial Refresh Pedicure 25 min, Refresh Manicure 25 min"};
+        awaken_your_senses = {Time: 100,Price: 300, Description:"50 min CBD Wellness Massage, 50 min Intense Hydrating Facial"};
+        just_the_beginning = {Time: 50, Price: 150, Description:"25 min Scalp, Neck, & Shoulder CBD Massage, 25 minute Refresh Facial"};
+        polished_and_pampered = {Time: 100, Price: 135, Description:"Classic Pedicure 50 min, Classic Manicure 50 min"};
+        wellnessRitual = {Time: 75, Price: 235, Description:"50 min CBD Wellness Massage, 25 minute Refresh Facial"};
+        your_worth_it = {Time: 200, Price: 410, Description:"50 Minute Aromatherapy Massage, 50 min Custom Facial, Classic Pedicure 50 min, Classic Manicure 50 min"};
+
+    }
+}
